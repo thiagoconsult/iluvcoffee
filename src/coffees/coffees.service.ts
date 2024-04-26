073915d119051a559abb4e11ff03a70caf4f90ad
thiagoconsult <thiago.consultx@gmail.com>
2024-04-26 19:34:47 -0300

    Alteração do repositório para postgres

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,58 +1,85 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entitie';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
-    private coffees: Coffee [] = [
-        {
-            id: 1,
-            name: "Shipwreck Roast",
-            brand: "Buddy Brew",
-            flavors: ['Vanilla', 'Chocolate']
-        }
-    ]
+    // private coffees: Coffee [] = [
+    //     {
+    //         id: 1,
+    //         name: "Shipwreck Roast",
+    //         brand: "Buddy Brew",
+    //         flavors: ['Vanilla', 'Chocolate']
+    //     }
+    // ]
 
-    private countId = 1;
+    // private countId = 1;
 
-    genId (){
-        this.countId ++;
-        return this.countId;
-    }
+    constructor(
+        @InjectRepository(Coffee)
+        private readonly coffeRepository: Repository<Coffee>,
+    ){}
+
+    // genId (){
+    //     this.countId ++;
+    //     return this.countId;
+    // }
 
     getAll(){
-        if(this.coffees.length === 0){
-            throw new NotFoundException(`No coffee found`)
-        }
-        return this.coffees;
+        return this.coffeRepository.find()
+        // if(this.coffees.length === 0){
+        //     throw new NotFoundException(`No coffee found`)
+        // }
+        // return this.coffees;
     }
 
-    findOne(id: string){
-        const coffee = this.coffees.find(item=> item.id === +id);
+    async findOne(id: string){
+        const coffee = await this.coffeRepository.findOne({where: {id: +id}})
         if(!coffee){
-            throw new NotFoundException(`Coffee #${id} not found`);
+            throw new NotFoundException(`Coffee #${id} not found`)
         }
-        return coffee;
+        return coffee
+        // const coffee = this.coffees.find(item=> item.id === +id);
+        // if(!coffee){
+        //     throw new NotFoundException(`Coffee #${id} not found`);
+        // }
+        // return coffee;
     }
 
-    create(createCoffeeDto: any){
-        createCoffeeDto.id = this.genId()
-        this.coffees.push(createCoffeeDto);
-        return createCoffeeDto;
+    create(createCoffeeDto: CreateCoffeeDto){
+        const coffee = this.coffeRepository.create(createCoffeeDto)
+        return this.coffeRepository.save(coffee);
+        // createCoffeeDto.id = this.genId()
+        // this.coffees.push(createCoffeeDto);
+        // return createCoffeeDto;
     }
 
-    update(id: string, updateCoffeeDto: any){
-        const existingCoffee = this.findOne(id)
-        if(existingCoffee){
-            //
+    async update(id: string, updateCoffeeDto: UpdateCoffeeDto){
+        const coffee = await this.coffeRepository.preload({
+            id: +id,
+            ...updateCoffeeDto
+        })
+        if(!coffee){
+            throw new NotFoundException(`Coffee #${id} not found`)
         }
+        return this.coffeRepository.save(coffee);
+        // const existingCoffee = this.findOne(id)
+        // if(existingCoffee){
+        //     //
+        // }
     }
 
-    remove(id: string){
-        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
-        if(coffeeIndex >= 0){
-            this.coffees.splice(coffeeIndex, 1)
-            return {message: `Coffee deleted.`};
-        }
-        throw new NotFoundException(`Coffe #${id} not found`)
+    async remove(id: string){
+        const coffee = await this.findOne(id)
+        return this.coffeRepository.remove(coffee);
+    //     const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
+    //     if(coffeeIndex >= 0){
+    //         this.coffees.splice(coffeeIndex, 1)
+    //         return {message: `Coffee deleted.`};
+    //     }
+    //     throw new NotFoundException(`Coffe #${id} not found`)
     }
 }
